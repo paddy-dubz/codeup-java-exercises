fa9262cb18dcb7db83bcc9ff7b43142bc296070d
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-11-14 09:27:27 -0600

    Updated DoWhile exercises to count down from 100 to -10 by 5's

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,9 +1,9 @@
 public class DoWhile {
     public static void main(String[] args) {
-        int counter = 0;
+        int counter = 100;
         do {
             System.out.println( " " + counter );
-            counter = counter + 2;
-        } while ( counter <= 100 );
+            counter = counter - 5;
+        } while ( counter >= -10 );
     }
 }
