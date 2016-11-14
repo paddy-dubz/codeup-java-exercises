06d0b9a8f796578f853871578332c2a963c8afa0
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-11-14 09:25:09 -0600

    Added While and DoWhile exercises

---
+++ src/While.java
@@ -0,0 +1,10 @@
+public class While {
+    public static void main(String[] args) {
+        int counter = 5;
+        while ( counter <= 15 )
+        {
+            System.out.println( " " + counter );
+            counter = counter + 1;
+        }
+    }
+}
