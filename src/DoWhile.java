8c326e7c65a21d15ad93d6a2fe061a0bb3295ec0
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-11-14 10:06:41 -0600

    Updated DoWhile to start at 2 and square each time until <= 1,000,000

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -1,9 +1,9 @@
 public class DoWhile {
     public static void main(String[] args) {
-        int counter = 100;
+        long counter = 2;
         do {
-            System.out.println( " " + counter );
-            counter = counter - 5;
-        } while ( counter >= -10 );
+            System.out.println(counter);
+            counter *= counter;
+        } while (counter <= 1000000);
     }
 }
