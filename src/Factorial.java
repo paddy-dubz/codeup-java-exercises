4a399328ec3019e5acc4597fa9e7f394ccc77c1d
Patrick Woolard <patrick.k.woolard@gmail.com>
2016-11-15 09:41:54 -0600

    Updatded Factorial exercise

--- src/Factorial.java
+++ src/Factorial.java
@@ -4,22 +4,26 @@ public class Factorial {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        String choice = "y";
+        while (choice.equalsIgnoreCase("y")) {
+            System.out.print("Enter a number from 1-10 to find the factorial: ");
+            Integer input = scanner.nextInt();
+            Long fact = 1L;
 
-        System.out.print("Enter a number from 1-10 to find the factorial: ");
-        int n = scanner.nextInt();
-
-        if (n <= 10) {
-
-            int result = 1;
-
-            for (int i = 1; i <= n; i++) {
-                result = result * i;
+            if (input < 0 || input > 10) {
+                System.out.print("You didn't listen to the instructions, goodbye!");
+                break;
             }
 
-            System.out.println(n + "!" + " = " + result);
+            for (int i = 1; i <= input; i++) {
+                fact += fact * i;
+            }
 
-        } else {
-            System.out.print("You didn't listen to the instructions, goodbye!");
+            System.out.println(input + "!" + " = " + fact);
+            System.out.print("Continue (y/n)?: ");
+            choice = scanner.next();
+
+
         }
     }
 }
